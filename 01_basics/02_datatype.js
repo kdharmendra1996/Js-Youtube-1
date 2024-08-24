d215ee438cfc1a1582efa7d71cc6dbb4b8d6220e
Dharmendra Kumar <153342103+kdharmendra1996@users.noreply.github.com>
2024-08-24 09:08:20 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs,not browser
+
+console.log(9
+    + 9
+) //code readabillity should be high
+
+let name="Dharmendra Kumar";
+let age=24
+let isLoggedIn=false;
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null => standalone value
+//undefined=>
+//symbol => unique
+
+//object
+
+console.log(typeof name)
